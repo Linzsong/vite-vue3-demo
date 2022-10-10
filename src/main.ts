835b1119e6242b9dfe3e373a9e61b7b0959218da
Arlene <mao18475979352@qq.com>
2022-10-10 23:44:53 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+// 引用.vue 文件时需要先声明这个.vue的类型
+import App from "./App.vue";
+createApp(App).mount("#app");
+
+// let a: number = '123'
+// console.log(a);
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue"; // 解析 .vue 文件
+export default defineConfig({
+  plugins: [vue()],
+});
