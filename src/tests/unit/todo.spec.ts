673169fff5946a12c8597d36dea051e3ee4e7c77
Arlene <mao18475979352@qq.com>
2022-10-27 23:00:51 +0800

    feat: 新增测试

--- src/tests/unit/todo.spec.ts
+++ src/tests/unit/todo.spec.ts
@@ -4,8 +4,7 @@
 
 // 测试的好处 可以保证核心功能能正常运行，可以生成使用的用法， 缺点就是得写，
 
-import Todo from "@/components/todo.vue"
-
+import Todo from "@/component/todo.vue"
 // vitest 测试框架 === jest
 // @vue/test-utils 提升测试编写的速度 js -> jq
 import { shallowMount, mount } from "@vue/test-utils"
@@ -22,23 +21,23 @@ describe("测试todo组件功能是否正常", () => {
     input.setValue("hello")
     expect(wrapper.vm.todo).toBe("hello")
   })
-  // it("用户点击按钮 新增一条数据 , 但是新增的内容不能为空", async () => {
-  //   const wrapper = mount(Todo)
-  //   const input = wrapper.find("input")
-  //   input.setValue("")
-  //   const button = wrapper.find("button")
-  //   await button.trigger("click")
-  //   expect(wrapper.findAll("li").length).toBe(0)
-  //   input.setValue("hello")
-  //   await button.trigger("click")
-  //   expect(wrapper.findAll("li").length).toBe(1)
-  // })
-  // it("用户点击按钮 新增一条数据 , 但是新增的内容不能为空", async () => {
-  //   const wrapper = mount(Todo)
-  //   const input = wrapper.find("input")
-  //   const button = wrapper.find("button")
-  //   input.setValue("hello")
-  //   await button.trigger("click")
-  //   expect(wrapper.find("li").text()).toBe("hello")
-  // })
+  it("用户点击按钮 新增一条数据 , 但是新增的内容不能为空", async () => {
+    const wrapper = mount(Todo)
+    const input = wrapper.find("input")
+    input.setValue("")
+    const button = wrapper.find("button")
+    await button.trigger("click")
+    expect(wrapper.findAll("li").length).toBe(0)
+    input.setValue("hello")
+    await button.trigger("click")
+    expect(wrapper.findAll("li").length).toBe(1)
+  })
+  it("用户点击按钮 新增一条数据 , 但是新增的内容不能为空", async () => {
+    const wrapper = mount(Todo)
+    const input = wrapper.find("input")
+    const button = wrapper.find("button")
+    input.setValue("hello")
+    await button.trigger("click")
+    expect(wrapper.find("li").text()).toBe("hello")
+  })
 })
